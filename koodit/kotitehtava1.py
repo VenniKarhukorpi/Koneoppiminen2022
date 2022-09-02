55ac746889cedd1abf359cdf685d332565288cc1
VenniKarhukorpi <t1kave01@students.oamk.fi>
2022-09-02 13:29:26 +0300

    jeebus

---
+++ koodit/kotitehtava1.py
@@ -0,0 +1,8 @@
+list = [1,2,3]
+print(list[1])
+
+tuple = (1,"Masa",3)
+print(tuple[1])
+
+dictionary = {"Make":111, "Masa":222, "Keke":333}
+print(dictionary["Masa"])
---
+++ koodit/kotitehtava2.py
@@ -0,0 +1,7 @@
+lista = []
+
+i=2
+while i < 10:
+    lista.append(i)
+    print(lista)
+    i+=1
